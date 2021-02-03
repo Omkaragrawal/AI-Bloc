c3ed8596cf5055b405650016f986acee94c0749d
aryanchugh816 <chugharyan816@gmail.com>
2021-02-03 21:39:08 +0530

    Added setting for cors headers

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'authentication',
     'drf_yasg',
@@ -59,6 +60,7 @@ SWAGGER_SETTINGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -146,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS=True
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
