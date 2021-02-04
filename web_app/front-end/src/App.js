fd2b6607d80276af25b0f293537e281ec5d2858c
aryanchugh816 <chugharyan816@gmail.com>
2021-02-04 15:03:54 +0530

    Revert "Temp Changes"
    This reverts commit fcabcb6988d258750aca5a9efd73c9574df5c0e3.

---
+++ web_app/front-end/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import MainComponent from './components/MainComponent'
+function App() {
+  return (
+
+    <BrowserRouter>
+      <MainComponent />
+    </BrowserRouter>
+  );
+}
+
+export default App;
